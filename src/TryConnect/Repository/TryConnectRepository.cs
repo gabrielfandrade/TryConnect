0e500b968d13c400f2a6ad78da02c379e34d157b
Gabriel Faustino de Andrade <gabrielfa.andrade@gmail.com>
2023-05-30 15:54:59 -0300

    CRUD de Posts

--- src/TryConnect/Repository/TryConnectRepository.cs
+++ src/TryConnect/Repository/TryConnectRepository.cs
@@ -11,9 +11,24 @@ namespace TryConnect.Repository
             _context = context;
         }
 
+        public void CreatePost(Post post)
+        {
+            _context.Posts.Add(post);
+            _context.SaveChanges();
+        }
+
         public void CreateStudent(Student student)
         {
             _context.Students.Add(student);
+            _context.SaveChanges();
+        }
+
+        public void DeletePost(Post post)
+        {
+            var entity = _context.Posts.First(p => p.PostId == post.PostId);
+
+            _context.Posts.Remove(entity);
+
             _context.SaveChanges();
         }
 
@@ -26,6 +41,21 @@ namespace TryConnect.Repository
             _context.SaveChanges();
         }
 
+        public Post? GetPostById(int id)
+        {
+            return _context.Posts.Find(id);
+        }
+
+        public IEnumerable<Post>? GetPosts()
+        {
+            return _context.Posts;
+        }
+
+        public IEnumerable<Post>? GetPostsByStudentId(int id)
+        {
+            return _context.Posts.Where(post => post.StudentId == id).ToList();
+        }
+
         public Student? GetStudentById(int id)
         {
             return _context.Students.Find(id);
@@ -34,6 +64,17 @@ namespace TryConnect.Repository
         public IEnumerable<Student>? GetStudents()
         {
             return _context.Students;
+        }
+
+        public void UpdatePost(Post post)
+        {
+            var entity = _context.Posts.First(p => p.PostId == post.PostId);
+
+            entity.Message = post.Message;
+            entity.Image = post.Image;
+            entity.UpdatedAt = post.UpdatedAt;
+
+            _context.SaveChanges();
         }
 
         public void UpdateStudent(Student student)
