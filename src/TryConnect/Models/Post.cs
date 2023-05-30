e3d118db427f93d870df48f070e7d992f425fb5c
Gabriel Faustino de Andrade <gabrielfa.andrade@gmail.com>
2023-05-30 15:54:04 -0300

    Modelo de Post

--- src/TryConnect/Models/Post.cs
+++ src/TryConnect/Models/Post.cs
@@ -1,12 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace TryConnect.Models
 {
     public class Post
     {
+        [Key]
         public int PostId { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public string? Image { get; set; }
         public string? Message { get; set; }
+        [ForeignKey("StudentId")]
         public int StudentId { get; set; }
         public virtual Student Student { get; set; }
         public ICollection<PostComment> Comments { get; set; }
