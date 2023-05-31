e0ff17f7f8914a48ae0bf403af985e4ac18c0dee
Gabriel Faustino de Andrade <gabrielfa.andrade@gmail.com>
2023-05-31 11:56:32 -0300

    Testes do Controller de Posts

--- src/TryConnect.Test/TryConnectPostTest.cs
+++ src/TryConnect.Test/TryConnectPostTest.cs
@@ -125,4 +125,117 @@ public class TryConnectPostTest : IClassFixture<TryConnectTestContext<Program>>
             }
         }
     };
+
+    [Fact(DisplayName = "POST /post Deve retornar BadRequest quando sem um post")]
+    public async Task ShouldBadRequestWithoutAPost()
+    {
+        var content = new StringContent(JsonConvert.SerializeObject(null), Encoding.UTF8, "application/json");
+
+        var response = await _client.PostAsync("post", content);
+
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+    }
+
+    [Theory(DisplayName = "PUT /post Deve atualizar um post existente")]
+    [MemberData(nameof(ShouldUpdateAPostData))]
+    public async Task ShouldUpdateAPost(Post post)
+    {
+        var stringContent = new StringContent(JsonConvert.SerializeObject(post), Encoding.UTF8, "application/json");
+
+        var response = await _client.PutAsync($"post/{post.PostId}", stringContent);
+
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+    }
+    public static readonly TheoryData<Post> ShouldUpdateAPostData = new()
+    {
+        new Post{
+            PostId = 1,
+            CreatedAt = DateTime.ParseExact("28/05/2023", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+            UpdatedAt = DateTime.ParseExact("31/05/2023", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+            Image = "https://upload.wikimedia.org/wikipedia/commons/4/4f/Csharp_Logo.png?20180210215736",
+            Message = "C# é o melhor de todos",
+            StudentId = 2,
+        }
+    };
+
+    [Fact(DisplayName = "PUT /post Deve retornar BadRequest quando sem um post")]
+    public async Task ShouldBadRequestWhenUpdateWithoutAStudent()
+    {
+        var content = new StringContent(JsonConvert.SerializeObject(null), Encoding.UTF8, "application/json");
+
+        var response = await _client.PutAsync("post/1", content);
+
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+    }
+
+    [Theory(DisplayName = "PUT /student Deve retornar NotFound quando sem um post")]
+    [MemberData(nameof(ShouldNotFoundUpdateData))]
+    public async Task ShouldNotFoundUpdate(Post post)
+    {
+        var content = new StringContent(JsonConvert.SerializeObject(post), Encoding.UTF8, "application/json");
+
+        var response = await _client.PutAsync($"post/{post.PostId}", content);
+
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+    }
+    public static readonly TheoryData<Post> ShouldNotFoundUpdateData = new()
+    {
+        new Post{
+            PostId = 4,
+            CreatedAt = DateTime.ParseExact("31/05/2023", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+            UpdatedAt = DateTime.ParseExact("31/05/2023", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+            Image = "https://uploads-ssl.webflow.com/6357d4aeff33e738c1d2c99a/63582ab932507b5fcdc8afd0_thumb.webp",
+            Message = "TMB é a melhor",
+            StudentId = 1,
+        }
+    };
+
+    [Theory(DisplayName = "DELETE /post Deve remover um post")]
+    [MemberData(nameof(ShouldDeleteAPostData))]
+    public async Task ShouldDeleteAPost(int id, List<Post> studentsExpected)
+    {
+        var response = await _client.DeleteAsync($"post/{id}");
+
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.NoContent);
+
+        var dbResponse = await _client.GetAsync("post");
+
+        var content = await dbResponse.Content.ReadAsStringAsync();
+
+        var result = JsonConvert.DeserializeObject<List<Post>>(content);
+
+        result.Should().BeEquivalentTo(studentsExpected);
+    }
+    public static readonly TheoryData<int, List<Post>> ShouldDeleteAPostData = new()
+    {
+    {
+        1,
+        new() {
+            new Post{
+                PostId = 2,
+                CreatedAt = DateTime.ParseExact("29/05/2023", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                UpdatedAt = DateTime.ParseExact("29/05/2023", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                Image = "https://codeopinion.com/wp-content/uploads/2017/10/Bitmap-MEDIUM_Entity-Framework-Core-Logo_2colors_Square_Boxed_RGB-300x300.png",
+                Message = "Entity Framework é o melhor",
+                StudentId = 3,
+            },
+            new Post{
+                PostId = 3,
+                CreatedAt = DateTime.ParseExact("30/05/2023", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                UpdatedAt = DateTime.ParseExact("30/05/2023", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                Image = "https://www.dbacorp.com.br/wp-content/uploads/2017/07/microsoft-sql-server-logo-300x163.png",
+                Message = "SQLServer é o melhor",
+                StudentId = 1,
+            },
+        }
+    }
+    };
+
+    [Fact(DisplayName = "DELETE /post Deve retornar NotFound quando quando sem um post")]
+    public async Task ShouldNotFoundDeleteAStudent()
+    {
+        var response = await _client.DeleteAsync("post/4");
+
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+    }
 }
